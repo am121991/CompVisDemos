f3cfd95e3c22c83425c7981b8f73c9a87d7f73f6
am121991 <amrinderjit9211@yahoo.co.uk>
2014-02-02 02:08:32 +0000

    implemented SUSAN edge detection (I hope)

--- SED.py
+++ SED.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-img = Image.open("img74.gif")
-img = img.convert('L') # convert to greyscale
+oimg = Image.open("img74.gif")
+img = oimg.convert('L') # convert to greyscale
 print("image size:", img.size)
 
 pixels = img.load()
@@ -17,3 +17,39 @@ for x in range(img.size[0] - 1):
 		pixels[x,y] = abs(2*pixels[x,y] - pixels[x+1,y] - pixels[x,y+1])
 
 img.show()
+
+# Susan edge detection with 3x3 mask
+
+def c(r, r0):
+	global t
+	if (abs(r - r0) > t):
+		return 0
+	else:
+		return 1
+
+img2 = oimg.convert('L')
+pixels2 = img2.load()
+
+t = 5
+n = [[0 for x in range(img2.size[1])] for x in range(img2.size[0])]
+g = 3*9/4
+
+for x in range(img2.size[0]):
+	for y in range(img2.size[1]):
+		for xx in range(x-1, x+2):
+			for yy in range(y-1, y+2):
+				if xx>=0 and xx<img2.size[0] and yy>=0 and yy<img2.size[1]:
+					n[x][y] += c(pixels[xx, yy], pixels[x,y])
+
+for x in range(img2.size[0]):
+	for y in range(img2.size[1]):
+		if n[x][y] < g:
+			n[x][y] = g - n[x][y]
+		else:
+			n[x][y] = 0
+
+for x in range(img2.size[0]):
+	for y in range(img2.size[1]):
+		pixels2[x,y] = n[x][y] * 255
+
+img2.show()
