688083f6b69f36061cac816487a73361eb6636f4
Daniel <dan@dan0.net>
2014-02-03 12:50:34 +0000

    Added new comparison function, radius mask

--- SED.py
+++ SED.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from math import *
 
 oimg = Image.open("img74.gif")
 img = oimg.convert('L') # convert to greyscale
@@ -27,19 +28,40 @@ def c(r, r0):
 	else:
 		return 1
 
-img2 = oimg.convert('L')
-pixels2 = img2.load()
+def c2(r, r0):
+	global t
+	return exp(-((r-r0)/t)**6)
 
-t = 5
-n = [[0 for x in range(img2.size[1])] for x in range(img2.size[0])]
-g = 3*9/4
+img2 = oimg.convert('L')
+img3 = oimg.convert('L')
+pixels2 = img2.load()
+pixels3 = img2.load()
+
+t = 10 								#threshold
+r = 3.4 							#mask radius
+md = int(ceil(3.4*2)) #mask dimension
+n = [[0 for x in range(img2.size[1])] for x in range(img2.size[0])]	#output
+m = [[0 for x in range(md)] for x in range(md)]											#mask
+count = 0							#mask count
+
+for x in range(md):
+	for y in range(md):
+		if (x-r+0.5)**2 + (y-r+0.5)**2 < r**2:
+			m[x][y] = 1
+			count += 1
+	print m[x]
+
+g = 3*count/4 				#geometric threshold
+print g, count
 
 for x in range(img2.size[0]):
 	for y in range(img2.size[1]):
-		for xx in range(x-1, x+2):
-			for yy in range(y-1, y+2):
-				if xx>=0 and xx<img2.size[0] and yy>=0 and yy<img2.size[1]:
-					n[x][y] += c(pixels[xx, yy], pixels[x,y])
+		for xr in range(md):
+			for yr in range(md):
+				xx = x-r+xr
+				yy = y-r+yr
+				if m[xr][yr] == 1 and xx>=0 and xx<img2.size[0] and yy>=0 and yy<img2.size[1]:
+					n[x][y] += c2(pixels[xx, yy], pixels[x,y])
 
 for x in range(img2.size[0]):
 	for y in range(img2.size[1]):
@@ -50,6 +72,8 @@ for x in range(img2.size[0]):
 
 for x in range(img2.size[0]):
 	for y in range(img2.size[1]):
-		pixels2[x,y] = n[x][y] * 255
+		pixels2[x,y] = n[x][y] * 255/27
 
 img2.show()
+
+
