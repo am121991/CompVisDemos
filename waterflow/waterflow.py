8a042a520051a6330ac7e95273d2e317e9b1da99
am121991 <amrinderjit9211@yahoo.co.uk>
2014-03-01 13:12:08 +0000

    Added convolution

--- waterflow/waterflow.py
+++ waterflow/waterflow.py
@@ -10,26 +10,70 @@ print("size: ", img.size)
 
 W = np.zeros(img.size)
 
-# Initialisation
-W[5, 250] = 1 #because why not
-
 q = Queue.Queue()
-q.put((5, 250))
 empty = False
 
+# Initialisation
+for x in range(3):
+	for y in range(3):
+		W[249+x, 4+y] = 1
+		q.put((249+x, 4+y))
+
+
+cmask = [
+		[
+			[-1, -1,  0],
+			[-1,  0,  1],
+			[ 0,  1,  1]
+		],[
+			[-1, -1, -1],
+			[ 0,  0,  0],
+			[ 1,  1,  1]
+		],[
+			[ 0, -1, -1],
+			[ 1,  0, -1],
+			[ 1,  1,  0]
+		],[
+			[ 1,  0, -1],
+			[ 1,  0, -1],
+			[ 1,  0, -1]
+		],[
+			[ 1,  1,  0],
+			[ 1,  0, -1],
+			[ 0, -1, -1]
+		],[
+			[ 1,  1,  1],
+			[ 0,  0,  0],
+			[-1, -1, -1]
+		],[
+			[ 0,  1,  1],
+			[-1,  0,  1],
+			[-1, -1,  0]
+		],[
+			[-1,  0,  1],
+			[-1,  0,  1],
+			[-1,  0 , 1]
+		]
+	]
 SPM = 3
+FDI = [0, 0, 0, 0, 0, 0, 0, 0]
 
 while empty == False:
 	try: 
 		## Contour element processing here
 		element = q.get(False)
 		print element
-
+		for i in range(8):
+			acc = 0.0
+			for x in range(3):
+				for y in range(3):
+					acc += W[element[0] + x - 1, element[1] + y - 1] * cmask[i][x][y]
+			FDI[i] = acc/float(SPM)
+		print FDI
 		## Put new water elements into queue
 	except:
 		empty = True
 
 
-imshow(data)
-gray()
+imshow(W)
 show()
